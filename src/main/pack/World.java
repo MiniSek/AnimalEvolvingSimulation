72b207a26a1717b0229a8eab6d3811824e9c7718
Dominik Sulik <72816824+MiniSek@users.noreply.github.com>
2020-10-19 17:56:38 +0200

    Poprawka

---
+++ src/main/pack/World.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+    }
+}
